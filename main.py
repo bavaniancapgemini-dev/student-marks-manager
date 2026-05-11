d8a93512e0ff2ae004ebc3e4b413ce56d38c6e4c
Bavani <bavaniancapgemini@gmail.com>
2026-05-11 11:29:11 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,25 @@
+students = {}
+
+while True:
+    print("\n1. Add Student")
+    print("2. View Students")
+    print("3. Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        name = input("Student name: ")
+        marks = input("Marks: ")
+        students[name] = marks
+        print("Student added!")
+
+    elif choice == "2":
+        print("\nStudent Records:")
+        for name, marks in students.items():
+            print(name, ":", marks)
+
+    elif choice == "3":
+        break
+
+    else:
+        print("Invalid choice")
